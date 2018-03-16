d379003289627006ae1f5f97b3215c29c3355a4e
Nathan <nateinthegame@gmail.com>
2018-03-15 19:19:02 -0700

    fix?

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -14,7 +14,7 @@ const getClientEnvironment = require('./env');
 
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
  
-const extractSass = new ExtractTextPlugin({
+const cssFilename = new ExtractTextPlugin({
     filename: "[name].[contenthash].css",
     disable: process.env.NODE_ENV === "development"
 });
@@ -41,7 +41,7 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/[name].[contenthash:8].css';
+//const cssFilename = 'static/css/[name].[contenthash:8].css';
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
