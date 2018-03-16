12d011e444643e00a762548dc1b373b77ddfd2cd
Nathan <nateinthegame@gmail.com>
2018-03-15 19:28:05 -0700

    Believe?

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -166,7 +166,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.scss$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -203,6 +203,12 @@ module.exports = {
                             flexbox: 'no-2009',
                           }),
                         ],
+                      },
+                    },
+                    {
+                      loader: "sass-loader",
+                      options: {
+                        modules: true,
                       },
                     },
                   ],
