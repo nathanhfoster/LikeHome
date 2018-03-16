3c20055244152be59c9db2e3598efb191e839410
Nathan <nateinthegame@gmail.com>
2018-03-15 18:44:14 -0700

    NM

---
+++ src/components/Explore.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {CollectionView} from './CollectionView.js';
+import Mainbar from './MainBar.js'
+
+const Explore = () => (
+  <div>
+    <Mainbar/>
+    <CollectionView/>
+  </div>
+);
+
+export default Explore;
